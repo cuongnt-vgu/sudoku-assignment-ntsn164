73322cffff94710744937d0de63519a3823b73f2
Cuong Nguyen <ntcuong2103@gmail.com>
2023-11-29 08:56:41 +0700

    added hidden single description

--- src/hidden_singles.h
+++ src/hidden_singles.h
@@ -8,5 +8,7 @@ typedef struct HiddenSingle_impl
     int value;
 } HiddenSingle;
 
-// find hidden single cells in a board, return the number of hidden single cells
-int hidden_singles(SudokuBoard *p_board);
+int find_hidden_single_values(Cell **p_cells, int *hidden_single_values);
+void find_hidden_single(Cell **p_cells, HiddenSingle *p_hidden_singles,
+                        int *p_counter);
+int hidden_singles(SudokuBoard *p_board, Cell ***p_solved_cells);
